991825d4eaa5ebbc64dd26e15790e0680dd063f6
Aron <aron-dosti@hotmail.com>
2023-05-23 13:17:04 +0200

    exbee test

--- Test arduino code/KleurHerkening/KleurHerkening.ino
+++ Test arduino code/KleurHerkening/KleurHerkening.ino
@@ -5,7 +5,7 @@ Zumo32U4LineSensors lijnSensoren;
 uint16_t lijnSensorWaarden[5];
 
 void setup() {
-  Serial.begin(9600);
+  Serial1.begin(9600);
   lijnSensoren.initFiveSensors();
 }
 
@@ -14,15 +14,15 @@ void loop() {
 
   for (uint8_t i = 0; i < 5; i++) {
     if (lijnSensorWaarden[i] < 700 && lijnSensorWaarden[i] > 470) {
-      Serial.print("grijs");
+      Serial1.print("grijs");
     }
     else {
-      Serial.print(lijnSensorWaarden[i]);
+      Serial1.print(lijnSensorWaarden[i]);
     }
-    Serial.print("\t");
+    Serial1.print("\t");
   }
 
-  Serial.println();
+  Serial1.println();
   
   delay(100);
 }
