55ad05a64952015f077e870ba0952762a046283f
Aron <aron-dosti@hotmail.com>
2023-05-31 22:48:02 +0200

    Update KleurHerkening.ino

--- Test arduino code/KleurHerkening/KleurHerkening.ino
+++ Test arduino code/KleurHerkening/KleurHerkening.ino
@@ -5,24 +5,35 @@ Zumo32U4LineSensors lijnSensoren;
 uint16_t lijnSensorWaarden[5];
 
 void setup() {
-  Serial1.begin(9600);
+  Serial.begin(9600);
   lijnSensoren.initFiveSensors();
 }
 
 void loop() {
   lijnSensoren.read(lijnSensorWaarden);
 
-  for (uint8_t i = 0; i < 5; i++) {
-    if (lijnSensorWaarden[i] < 700 && lijnSensorWaarden[i] > 470) {
-      Serial1.print("grijs");
+  for (uint8_t i = 0; i < 2; i > 2; i < 5; i++) {
+    // if(i = 0 || i = 1){
+    while(lijnSensorWaarden[i] < 560 && lijnSensorWaarden[i] > 546 && lijnSensorWaarden[i] < 380 && lijnSensorWaarden[i] > 376 && lijnSensorWaarden[i] < 322 && lijnSensorWaarden[i] > 322 && lijnSensorWaarden[i] < 612 && lijnSensorWaarden[i] > 608) {
+      Serial.print("grijs");
+      //snelheid(0);
+      //timer;
+      return;
+    }
+    //snelheid(200);
+    // else if(i = 3 || i = 4) {
+    //   while() {
+    //     Serial.print("grijs");
+    //   }
+    // }
     }
     else {
-      Serial1.print(lijnSensorWaarden[i]);
+      Serial.print(lijnSensorWaarden[i]);
     }
-    Serial1.print("\t");
+    Serial.print("\t");
   }
 
-  Serial1.println();
+  Serial.println();
   
   delay(100);
 }
