d9619befaeed3c5c5a9cc4299dcb42b459f15f47
Aron <aron-dosti@hotmail.com>
2023-06-04 17:44:11 +0200

    test van stopfunctie bij grijslijn

--- Test arduino code/KleurHerkening/KleurHerkening.ino
+++ Test arduino code/KleurHerkening/KleurHerkening.ino
@@ -11,16 +11,17 @@ void setup() {
 
 void loop() {
   lijnSensoren.read(lijnSensorWaarden);
-
-for (int i = 0; i < 5; i++) {
-    if (i != 2) {
-        while(lijnSensorWaarden[i] < 560 && lijnSensorWaarden[i] > 546 && lijnSensorWaarden[i] < 380 && lijnSensorWaarden[i] > 376 && lijnSensorWaarden[i] < 322 && lijnSensorWaarden[i] > 322 && lijnSensorWaarden[i] < 612 && lijnSensorWaarden[i] > 608) {
-      Serial.print("grijs");
+  int senslinks = lijnSensorWaarden[0];
+  int sensrechts = lijnSensorWaarden[4];
+// for (int i = 0; i < 5; i++) {
+    // if (i != 2) {
+        // while(lijnSensorWaarden[i] < 560 && lijnSensorWaarden[i] > 546 && lijnSensorWaarden[i] < 380 && lijnSensorWaarden[i] > 376 && lijnSensorWaarden[i] < 322 && lijnSensorWaarden[i] > 322 && lijnSensorWaarden[i] < 612 && lijnSensorWaarden[i] > 608) {
+      // Serial.print("ik zie grijs! stooooooooooppp!!!!!!");
       //snelheid(0);
       //timer;
-      return;
-    }    
-    }
+      // return;
+    // }    
+    // }
     // if(i = 0 || i = 1){
 
     //snelheid(200);
@@ -30,13 +31,13 @@ for (int i = 0; i < 5; i++) {
     //   }
     // }
     
-    else {
-      Serial.print(lijnSensorWaarden[i]);
-    }
-    Serial.print("\t");
-  }
+    // else {
+      Serial.print(senslinks);
+    // }
+    // Serial.print("\t");
+  // }
 
-  Serial.println();
+  // Serial.println();
   
   delay(100);
 }
