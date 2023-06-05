48a97871748f3eb96a68bb178f869c5c2964070c
scxtyy <akt1238karim@gmail.com>
2023-06-05 13:45:50 +0200

    ziggy 90 graaden ding werkt waarschijnlijk niet heb het snel gedaan

---
+++ Test arduino code/Test bocht/Zumo  bochttest/Zumo/LijnVolgen/LijnVolgen.ino
@@ -0,0 +1,61 @@
+#include "LijnVolgen.h"
+
+
+LijnVolgen::LijnVolgen() : lastError(0), error(0), maxSpeed(300) {}
+
+void LijnVolgen::init()
+{
+  lijnsensor.initialiseren();
+  lijnsensor.kalibreren(motoren);
+}
+
+void LijnVolgen::start()
+{
+  //kijk of er een bocht gemaakt moet worden
+  if (lijnsensor.sensorWaarden[0] > 900 || lijnsensor.sensorWaarden[4] > 900) 
+  {
+    //kijk of we naar links of rechts moeten
+    if (volgendebocht == "links") 
+    {
+      motoren.rijRechts(100);
+      volgendebocht = "niks";
+    }
+    if (volgendebocht == "rechts")
+    {
+      motoren.rijLinks(100);
+      volgendebocht = "niks";
+    }
+  }
+  else 
+  {
+    error = lijnsensor.error();
+    int16_t snelheidsVerschil = error / 4 + 6 * (error - lastError);
+    lastError = error;
+
+    int16_t snelheidLinks = maxSpeed + snelheidsVerschil;
+    int16_t snelheidRechts = maxSpeed - snelheidsVerschil;
+
+    snelheidLinks = constrain(snelheidLinks, -maxSpeed, maxSpeed);
+    snelheidRechts = constrain(snelheidRechts, -maxSpeed, maxSpeed);
+
+    motoren.setSpeeds(snelheidLinks, snelheidRechts);
+  }
+}  
+
+void LijnVolgen::kiesBocht() {
+  if (lijnsensor.sensorWaarden[0] < 900 && lijnsensor.sensorWaarden[0] > 220) 
+  {
+    volgendebocht = "links";
+    Serial.println(volgendebocht);
+  }
+  else if (lijnsensor.sensorWaarden[4] < 1000 && lijnsensor.sensorWaarden[4] > 200) 
+  {
+    volgendebocht = "rechts";
+    Serial.println(volgendebocht);
+  } 
+  else
+  {
+
+  }
+}
+
--- Test arduino code/Test bocht/Zumo  bochttest/Zumo/Motoren.h
+++ Test arduino code/Test bocht/Zumo  bochttest/Zumo/Motoren.h
@@ -13,10 +13,13 @@ public:
   void rijRechts(int snelheid);
   void draaiLinks(int snelheid);
   void draaiRechts(int snelheid);
-  void setSpeeds(int, int);
+  void setSpeeds(int links, int rechts);
+  void draai90links();
+  void draai90rechts();
 
 private:
   Zumo32U4Motors motors;
+  Zumo32U4Encoders encoders;
 };
 
 #endif
