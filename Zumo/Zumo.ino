22fdd9530af0117d5f3387bfe885ec7e9b2d34c5
Dialpr <125963156+Dialpr@users.noreply.github.com>
2023-06-06 10:20:40 +0200

    Update Zumo.ino

--- Zumo/Zumo.ino
+++ Zumo/Zumo.ino
@@ -3,28 +3,14 @@
 // Gebruik voor seriële communicatie: #include <Wire.h>
 
 LijnVolgen lijnVolgen;;
-Motoren motoren;
 
 void setup() 
 {
-  // Gebruik voor seriële communicatie: Serial.begin(9600);
-  // Wire.begin();
-  delay(1000);
-  //ijnVolgen.init();
-     motoren.draai90rechts();
-     delay(3000);
-     motoren.draai90links();
-    motoren.stap();
+  lijnVolgen.init();
 }
 
 void loop() 
 {
- /* if (lijnVolgen.volgendeBocht == "")
-  {
-    lijnVolgen.bocht_registratie();
-  }
+  lijnVolgen.bocht_registratie();
   lijnVolgen.standaardModus();
-  */
-
-  
 }
