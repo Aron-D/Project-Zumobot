c6a0d59c29ea35486a3f50517ea3ea3a4169ffda
scxtyy <akt1238karim@gmail.com>
2023-06-06 10:17:04 +0200

    stap toegevoegd

--- Zumo/Zumo.ino
+++ Zumo/Zumo.ino
@@ -1,21 +1,30 @@
 #include "LijnVolgen.h"
+#include "Motoren.h"
 // Gebruik voor seriële communicatie: #include <Wire.h>
 
-LijnVolgen lijnVolgen;
+LijnVolgen lijnVolgen;;
+Motoren motoren;
 
 void setup() 
 {
   // Gebruik voor seriële communicatie: Serial.begin(9600);
   // Wire.begin();
   delay(1000);
-  lijnVolgen.init();
+  //ijnVolgen.init();
+     motoren.draai90rechts();
+     delay(3000);
+     motoren.draai90links();
+    motoren.stap();
 }
 
 void loop() 
 {
-  if (lijnVolgen.volgendeBocht == "")
+ /* if (lijnVolgen.volgendeBocht == "")
   {
     lijnVolgen.bocht_registratie();
   }
   lijnVolgen.standaardModus();
+  */
+
+  
 }
